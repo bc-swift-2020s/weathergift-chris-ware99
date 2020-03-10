ae2575e4ee2c94c13e82e8bd952026ae3061d1c0
Christopher Ware <warech@bc.edu>
2020-03-10 13:45:25 -0400

    blank line

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Chris Ware on 3/10/20.
+//  Copyright © 2020 Chris Ware. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
