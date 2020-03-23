f6e8c9cd4336ddcef2cfc17ebc88e0691333b886
Christopher Ware <warech@bc.edu>
2020-03-23 16:54:45 -0400

    blank line

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct WeatherLocation: Codable {
+class WeatherLocation: Codable {
     var name: String
     var latitude: Double
     var longitude: Double
@@ -19,36 +19,5 @@ struct WeatherLocation: Codable {
         self.longitude = longitude
     }
     
-    func getData() {
-        let coordinates = "\(latitude),\(longitude)"
-        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkyKey)/\(coordinates)"
-        print("We are accessing \(urlString)")
-        
-        // Create a URL
-        guard let url = URL(string: urlString) else {
-            print("ERROR: Could not create a URL from \(urlString)")
-            return
-        }
-        
-        // Create Session
-        let session = URLSession.shared
-        
-        // Get data with .dataTask method
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("ERROR: \(error.localizedDescription)")
-            }
-            
-            // deal with the data
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print("\(json)")
-            } catch {
-                print("JSON ERROR: \(error.localizedDescription)")
-            }
-            
-        }
-        
-        task.resume()
-    }
+
 }
