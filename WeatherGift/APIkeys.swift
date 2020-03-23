ceed2af9a1315af3a8e7b85129939e57d9332f65
Christopher Ware <warech@bc.edu>
2020-03-23 13:23:11 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Chris Ware on 3/23/20.
+//  Copyright © 2020 Chris Ware. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBwAVdNq9VCYrqxsabsEzs-1d_35aiVxoU"
+}
